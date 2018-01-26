da63d4ddffda95f5047bb1d62cc290b09c41c78e
HannaJeon <jks7psj@gmail.com>
2018-01-26 14:56:09 +0900

    질문 상세 보여주기

--- src/main/java/com/woowahan/firstweb/question/QuestionController.java
+++ src/main/java/com/woowahan/firstweb/question/QuestionController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -32,6 +33,13 @@ public class QuestionController {
 	public String create(Question question) {
 		questionRepository.save(question);
 		return "redirect:/";
+	}
+
+	@GetMapping("/{id}")
+	public String show(@PathVariable long id, Model model) {
+		Question question = questionRepository.findOne(id);
+		model.addAttribute(question);
+		return "question/show";
 	}
 
 }
